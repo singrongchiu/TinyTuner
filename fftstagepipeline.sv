2083b6646b3f97aaab84de93261bd5aa540e0d50
singrongchiu <singrongchiu@cmu.edu>
2025-04-29 20:50:26 -0400

    working fft

--- fftstagepipeline.sv
+++ fftstagepipeline.sv
@@ -91,37 +91,37 @@ module Radix2FFTPipeline #(
   
     always_ff @(negedge rst_n) begin
       twiddle_real[0] = 8'sd64; twiddle_imag[0] = 8'sd0;
-      twiddle_real[1] = 8'sd64; twiddle_imag[1] = 8'sd6;
-      twiddle_real[2] = 8'sd63; twiddle_imag[2] = 8'sd12;
-      twiddle_real[3] = 8'sd61; twiddle_imag[3] = 8'sd19;
-      twiddle_real[4] = 8'sd59; twiddle_imag[4] = 8'sd24;
-      twiddle_real[5] = 8'sd56; twiddle_imag[5] = 8'sd30;
-      twiddle_real[6] = 8'sd53; twiddle_imag[6] = 8'sd36;
-      twiddle_real[7] = 8'sd49; twiddle_imag[7] = 8'sd41;
-      twiddle_real[8] = 8'sd45; twiddle_imag[8] = 8'sd45;
-      twiddle_real[9] = 8'sd41; twiddle_imag[9] = 8'sd49;
-      twiddle_real[10] = 8'sd36; twiddle_imag[10] = 8'sd53;
-      twiddle_real[11] = 8'sd30; twiddle_imag[11] = 8'sd56;
-      twiddle_real[12] = 8'sd24; twiddle_imag[12] = 8'sd59;
-      twiddle_real[13] = 8'sd19; twiddle_imag[13] = 8'sd61;
-      twiddle_real[14] = 8'sd12; twiddle_imag[14] = 8'sd63;
-      twiddle_real[15] = 8'sd6; twiddle_imag[15] = 8'sd64;
-      twiddle_real[16] = 8'sd0; twiddle_imag[16] = 8'sd64;
-      twiddle_real[17] = -8'sd6; twiddle_imag[17] = 8'sd64;
-      twiddle_real[18] = -8'sd12; twiddle_imag[18] = 8'sd63;
-      twiddle_real[19] = -8'sd19; twiddle_imag[19] = 8'sd61;
-      twiddle_real[20] = -8'sd24; twiddle_imag[20] = 8'sd59;
-      twiddle_real[21] = -8'sd30; twiddle_imag[21] = 8'sd56;
-      twiddle_real[22] = -8'sd36; twiddle_imag[22] = 8'sd53;
-      twiddle_real[23] = -8'sd41; twiddle_imag[23] = 8'sd49;
-      twiddle_real[24] = -8'sd45; twiddle_imag[24] = 8'sd45;
-      twiddle_real[25] = -8'sd49; twiddle_imag[25] = 8'sd41;
-      twiddle_real[26] = -8'sd53; twiddle_imag[26] = 8'sd36;
-      twiddle_real[27] = -8'sd56; twiddle_imag[27] = 8'sd30;
-      twiddle_real[28] = -8'sd59; twiddle_imag[28] = 8'sd24;
-      twiddle_real[29] = -8'sd61; twiddle_imag[29] = 8'sd19;
-      twiddle_real[30] = -8'sd63; twiddle_imag[30] = 8'sd12;
-      twiddle_real[31] = -8'sd64; twiddle_imag[31] = 8'sd6;
+      twiddle_real[1] = 8'sd64; twiddle_imag[1] = -8'sd6;
+      twiddle_real[2] = 8'sd63; twiddle_imag[2] = -8'sd12;
+      twiddle_real[3] = 8'sd61; twiddle_imag[3] = -8'sd19;
+      twiddle_real[4] = 8'sd59; twiddle_imag[4] = -8'sd24;
+      twiddle_real[5] = 8'sd56; twiddle_imag[5] = -8'sd30;
+      twiddle_real[6] = 8'sd53; twiddle_imag[6] = -8'sd36;
+      twiddle_real[7] = 8'sd49; twiddle_imag[7] = -8'sd41;
+      twiddle_real[8] = 8'sd45; twiddle_imag[8] = -8'sd45;
+      twiddle_real[9] = 8'sd41; twiddle_imag[9] = -8'sd49;
+      twiddle_real[10] = 8'sd36; twiddle_imag[10] = -8'sd53;
+      twiddle_real[11] = 8'sd30; twiddle_imag[11] = -8'sd56;
+      twiddle_real[12] = 8'sd24; twiddle_imag[12] = -8'sd59;
+      twiddle_real[13] = 8'sd19; twiddle_imag[13] = -8'sd61;
+      twiddle_real[14] = 8'sd12; twiddle_imag[14] = -8'sd63;
+      twiddle_real[15] = 8'sd6; twiddle_imag[15] = -8'sd64;
+      twiddle_real[16] = 8'sd0; twiddle_imag[16] = -8'sd64;
+      twiddle_real[17] = -8'sd6; twiddle_imag[17] = -8'sd64;
+      twiddle_real[18] = -8'sd12; twiddle_imag[18] = -8'sd63;
+      twiddle_real[19] = -8'sd19; twiddle_imag[19] = -8'sd61;
+      twiddle_real[20] = -8'sd24; twiddle_imag[20] = -8'sd59;
+      twiddle_real[21] = -8'sd30; twiddle_imag[21] = -8'sd56;
+      twiddle_real[22] = -8'sd36; twiddle_imag[22] = -8'sd53;
+      twiddle_real[23] = -8'sd41; twiddle_imag[23] = -8'sd49;
+      twiddle_real[24] = -8'sd45; twiddle_imag[24] = -8'sd45;
+      twiddle_real[25] = -8'sd49; twiddle_imag[25] = -8'sd41;
+      twiddle_real[26] = -8'sd53; twiddle_imag[26] = -8'sd36;
+      twiddle_real[27] = -8'sd56; twiddle_imag[27] = -8'sd30;
+      twiddle_real[28] = -8'sd59; twiddle_imag[28] = -8'sd24;
+      twiddle_real[29] = -8'sd61; twiddle_imag[29] = -8'sd19;
+      twiddle_real[30] = -8'sd63; twiddle_imag[30] = -8'sd12;
+      twiddle_real[31] = -8'sd64; twiddle_imag[31] = -8'sd6;
     end
   
     generate
@@ -147,6 +147,9 @@ module Radix2FFTPipeline #(
         // note, had to watch out for bit reversal
         always_ff @(posedge clk or negedge rst_n) begin
           if (stage_valid[s]) begin
+//             $display("stage %d fft layer", s);
+//             $display(stage_real[s]);
+//             $display(stage_imag[s]);
           for (int group = 0; group < (2**s); group++) begin : fft_group
             for (int pair = 0; pair < N / (2**(s+1)); pair++) begin : fft_pair
 
@@ -176,22 +179,22 @@ module Radix2FFTPipeline #(
 //                 prod_imag[s] = (b_real[s] * twiddle_imag[twiddle_index[s]] + b_imag[s] * twiddle_real[twiddle_index[s]]) >>> (TWIDDLE_WIDTH - 1);
 //               prod_real[s] = (b_real[s] * twiddle_real[twiddle_index[s]] - b_imag[s] * twiddle_imag[twiddle_index[s]]);
 //               prod_imag[s] = (b_real[s] * twiddle_imag[twiddle_index[s]] + b_imag[s] * twiddle_real[twiddle_index[s]]);
-              prod_real[s] = (abdiff_real[s] * twiddle_real[twiddle_index[s]] - abdiff_imag[s] * twiddle_imag[twiddle_index[s]]);
-              prod_imag[s] = (abdiff_real[s] * twiddle_imag[twiddle_index[s]] + abdiff_imag[s] * twiddle_real[twiddle_index[s]]);
+              prod_real[s] = (abdiff_real[s] * twiddle_real[twiddle_index[s]] - abdiff_imag[s] * twiddle_imag[twiddle_index[s]]) >>> (TWIDDLE_WIDTH - 2);
+              prod_imag[s] = (abdiff_real[s] * twiddle_imag[twiddle_index[s]] + abdiff_imag[s] * twiddle_real[twiddle_index[s]]) >>> (TWIDDLE_WIDTH - 2);
 //               $display("s %d b_real %d", s, b_real[s]);
 //               $display("s %d a_real %d", s, a_real[s]);
 //               $display("s %d b_imag %d", s, b_imag[s]);
 //               $display("s %d a_imag %d", s, a_imag[s]);
 //               $display("s %d twiddle_real[index] %d", s, twiddle_real[twiddle_index[s]]);
 //               $display("s %d twiddle_imag[index] %d", s, twiddle_imag[twiddle_index[s]]);
-//               $display("s %d prod_real %d", s, prod_real[s]);
+              //               $display("s %d prod_real %d", s, prod_real[s]);
 //               $display("s %d prod_imag %d", s, prod_imag[s]);
 
                 // Butterfly
-                stage_real[s+1][idx_a[s]] <= (a_real[s] + b_real[s])/2;
-                stage_imag[s+1][idx_a[s]] <= (a_imag[s] + b_imag[s])/2;
-                stage_real[s+1][idx_b[s]] <= (prod_real[s])/2;
-                stage_imag[s+1][idx_b[s]] <= (prod_imag[s])/2;
+              stage_real[s+1][idx_a[s]] <= (a_real[s] + b_real[s]);
+              stage_imag[s+1][idx_a[s]] <= (a_imag[s] + b_imag[s]);
+              stage_real[s+1][idx_b[s]] <= (prod_real[s]);
+              stage_imag[s+1][idx_b[s]] <= (prod_imag[s]);
 //               stage_real[s+1][group * (2**(s+1)) + pair] <= a_real[s][group] + prod_real[s];
 //               stage_imag[s+1][group * (2**(s+1)) + pair] <= a_imag[s][group] + prod_imag[s];
 //               stage_real[s+1][group * (2**(s+1)) + pair + (2**s)] <= a_real[s][group] - prod_real[s];
@@ -204,8 +207,8 @@ module Radix2FFTPipeline #(
     endgenerate
 
     logic [STAGES-1:0] max_bin;
-    logic signed [DATA_WIDTH*3:0] max_magnitude;
-    logic signed [DATA_WIDTH*3:0] current_magnitude;
+  logic signed [DATA_WIDTH*2:0] max_magnitude;
+  logic signed [DATA_WIDTH*2:0] current_magnitude;
     
     // Output
     always_ff @(posedge clk or negedge rst_n) begin
@@ -223,7 +226,7 @@ module Radix2FFTPipeline #(
           $display(stage_real[STAGES]);
           $display(stage_imag[STAGES]);
           
-          for (int i = 0; i < N; i++) begin
+          for (int i = N-1; i > 0; i--) begin
             current_magnitude = ((stage_real[STAGES][i])*(stage_real[STAGES][i])) + ((stage_imag[STAGES][i])*(stage_imag[STAGES][i]));
 //             $display("i: %d", i);
 //             $display(stage_real[STAGES][i]);
@@ -236,7 +239,7 @@ module Radix2FFTPipeline #(
 //             $display(current_magnitude);
 //             $display("highest_bin");
 //             $display(max_bin);
-            if (current_magnitude > max_magnitude) begin
+            if (current_magnitude >= max_magnitude) begin
               max_bin = i;
               max_magnitude = current_magnitude;
             end
@@ -245,6 +248,9 @@ module Radix2FFTPipeline #(
           out_valid <= 1'b1;
           $display("highest_bin: %d", max_bin);
           $display("highest_magnitude: %d", max_magnitude);
+          $display("stage 6! fft layer");
+          $display(stage_real[6]);
+          $display(stage_imag[6]);
         end else begin
 //           $display(stage_valid);
           out_valid <= 1'b0;
