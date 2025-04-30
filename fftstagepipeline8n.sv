350cf6daa9a6ac5e04155d410f836fd4adb7e025
singrongchiu <singrongchiu@cmu.edu>
2025-04-30 01:53:45 -0400

    cleanup

--- fftstagepipeline8n.sv
+++ fftstagepipeline8n.sv
@@ -1,4 +1,4 @@
-module Radix2FFTPipeline #(
+module Radix2FFTPipeline8N #(
     parameter DATA_WIDTH = 8,
     parameter TWIDDLE_WIDTH = 8,
     parameter N = 8,
@@ -303,11 +303,13 @@ module Radix2FFTPipeline #(
           end
           highest_bin <= max_bin;
           out_valid <= 1'b1;
+          /*
           $display("highest_bin: %d", max_bin);
           $display("highest_magnitude: %d", max_magnitude);
           $display("stage 3! fft layer");
           $display(stage_real3);
           $display(stage_imag3);
+          */
         end else begin
 //           $display(stage_valid);
           out_valid <= 1'b0;
